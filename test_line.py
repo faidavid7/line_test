6c0f5d518260897a924245d2ac2497719695dfb8
faidavid7 <111627118+faidavid7@users.noreply.github.com>
2023-10-19 13:05:08 +0800

    Create test_line.py

---
+++ test_line.py
@@ -0,0 +1,63 @@
+# test_line.py
+
+from bs4 import BeautifulSoup
+from datetime import datetime
+import requests
+import time
+
+url = "https://powerapi.tsinghua.tw/n1";
+status = -1
+i = 0
+while(1):
+  response = requests.get(url);
+  data = response.json();
+  print("Current time:", data['time'], " ", "Current Load:", data['value'], "kW");
+  
+  pre_status = status
+  if(data['value'] < 6700):
+    status = 0
+  if (data['value'] > 6700 and data['value'] < 6800):
+    status = 1
+  if (data['value'] >= 6800 and data['value'] < 6850):
+    status = 2
+  if (data['value'] >= 6850 and data['value'] < 6900):
+    status = 3
+  if (data['value'] > 6900):
+    status = 4
+  print(status, pre_status)
+  if (status == 0 and pre_status != 0):
+    headers = {
+      "Authorization": "Bearer " + "mVO97RfsTkgQHmbgC2jcS2NznBbeo5825HgNu2RKChR",
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+    params = {"message": f"\n低於6700 kW\n資料提取時間:\n {data['time']}\n瓦數:\n{data['value']} kW"}
+    requests.post("https://notify-api.line.me/api/notify", headers=headers, params=params)
+  if (status == 1 and pre_status != 1):
+      headers = {
+        "Authorization": "Bearer " + "mVO97RfsTkgQHmbgC2jcS2NznBbeo5825HgNu2RKChR",
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+      params = {"message": f"\n超過6700 kW\n資料提取時間:\n {data['time']}\n瓦數:\n{data['value']} kW"}
+      requests.post("https://notify-api.line.me/api/notify", headers=headers, params=params)
+  if (status == 2 and pre_status != 2):
+      headers = {
+        "Authorization": "Bearer " + "mVO97RfsTkgQHmbgC2jcS2NznBbeo5825HgNu2RKChR",
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+      params = {"message": f"\n超過6800 kW\n資料提取時間:\n {data['time']}\n瓦數:\n{data['value']} kW"}
+      requests.post("https://notify-api.line.me/api/notify", headers=headers, params=params)
+  if (status == 3 and pre_status != 3):
+      headers = {
+        "Authorization": "Bearer " + "mVO97RfsTkgQHmbgC2jcS2NznBbeo5825HgNu2RKChR",
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+      params = {"message": f"\n超過6850 kW\n資料提取時間:\n {data['time']}\n瓦數:\n{data['value']} kW"}
+      requests.post("https://notify-api.line.me/api/notify", headers=headers, params=params)
+  if (status == 4 and pre_status != 4):
+      headers = {
+        "Authorization": "Bearer " + "mVO97RfsTkgQHmbgC2jcS2NznBbeo5825HgNu2RKChR",
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+      params = {"message": f"\n超過6900 kW\n資料提取時間:\n {data['time']}\n瓦數:\n{data['value']} kW"}
+      requests.post("https://notify-api.line.me/api/notify", headers=headers, params=params)
+  time.sleep(60)
